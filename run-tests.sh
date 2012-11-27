ad430a69fb84ab01563618a8f56e39c1737877af
Remi Collet <fedora@famillecollet.com>
2012-11-27 16:16:34 +0100

    test result with valgrind-3.8.1, Tests leaked: 0  :)

--- run-tests.sh
+++ run-tests.sh
@@ -6,4 +6,7 @@ TEST_PHP_EXECUTABLE=$(which php) \
 TEST_PHP_ARGS="-n -d extension_dir=$PWD/modules -d extension=json.so" \
 NO_INTERACTION=1 \
 REPORT_EXIT_STATUS=1 \
-php -n run-tests.php | sed -e 's/^TEST.*\x0d//' | tee STATUS
+php -n run-tests.php $* | tee STATUS
+
+sed -e 's/^TEST.*\x0d//' -i STATUS
+
