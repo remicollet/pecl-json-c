e4c0e205b624d76db6064170083b73e916e30e82
Remi Collet <fedora@famillecollet.com>
2014-03-21 18:06:20 +0100

    bench result, with jsond results

--- bench.php
+++ bench.php
@@ -1,17 +1,29 @@
 <?php
 echo "\nPHP  Version: ".phpversion()."\n";
-if (version_compare(phpversion('json'), '1.3.0', '<')) {
-	$ver = "ext/json ".phpversion('json');
+if (extension_loaded('jsond')) {
+	$ver = "pecl/jsond ".phpversion('jsond');
+	$suf = "jsond";
+	$enc = 'jsond_encode';
+	$dec = 'jsond_decode';
+	$opt = JSOND_PRETTY_PRINT;
+} else if (version_compare(phpversion('json'), '1.3.0', '<')) {
+	$ver = "ext/json   ".phpversion('json');
 	$suf = "json";
+	$enc = 'json_encode';
+	$dec = 'json_decode';
+	$opt = JSON_PRETTY_PRINT;
 } else {
 	$ver = "pecl/jsonc ".phpversion('json');
 	$suf = "jsonc";
+	$enc = 'json_encode';
+	$dec = 'json_decode';
+	$opt = JSON_PRETTY_PRINT;
 }
 
 echo "Json Version: ".$ver."\n\n";
 
 $ary = get_loaded_extensions();
-for($i=0; $i<pow(2, 7); $i++){
+for($i=0; $i<pow(2, 8); $i++){
 	// integers
 	$ary = array_merge($ary, range(0, 1024));
 	// strings
@@ -23,14 +35,14 @@ for($i=0; $i<pow(2, 7); $i++){
 echo "Count: ".count($ary)."\n";
 
 $a = microtime(true);
-$jsoned = json_encode($ary);
+$jsoned = $enc($ary);
 $b = microtime(true);
 printf("Encode in %.5f sec, %ld bytes\n", $b-$a, strlen($jsoned));
 
-file_put_contents("bench.$suf", json_encode($ary, JSON_PRETTY_PRINT));
+file_put_contents("bench.$suf", $enc($ary, $opt));
 
 $a = microtime(true);
-$json = json_decode($jsoned);
+$json = $dec($jsoned);
 $b = microtime(true);
 if ($json) {
     printf("Decode in %.5f sec\n", $b-$a);
